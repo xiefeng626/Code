8a333802031ed2b549ee76ea01a693927f8058e1
zhao1 <eda123@qq.com>
2020-11-09 19:43:25 +0800

    2020-11-9 001

--- BaseCode/src/main/java/DesignPattern/factory/abstractfactory/AbstractFactoryTest.java
+++ BaseCode/src/main/java/DesignPattern/factory/abstractfactory/AbstractFactoryTest.java
@@ -1,19 +1,24 @@
 package DesignPattern.factory.abstractfactory;
+/**
+ * 工厂方法
+ */
 
-import org.junit.Test;
-
-public class AbstractFactory {
-    @Test
-    public void maintest() {
-        AbstractFactorys abstractFactorys = new ConcreteFactory1();
-        Product product = abstractFactorys.newProduct();
-        product.show();
+/**
+ * 选择合适的工厂创建产品
+ * 节我们介绍了简单工厂模式，提到了简单工厂模式违背了开闭原则，而“工厂方法模式”是对简单工厂模式的进一步抽象化，其好处是可以使系统在不修改原来代码的情况下引进新的产品，即满足开闭原则。
+ */
+public class AbstractFactoryTest {
+    public static void main(String[] args) {
+        Product a;
+        //选择 可以通过配置文件来
+        AbstractFactory af = new ConcreteFactory1();
+        a = af.newProduct();
+        a.show();
     }
 }
-
 //抽象产品：提供了产品的接口
 interface Product {
-     void show();
+    public void show();
 }
 //具体产品1：实现抽象产品中的抽象方法
 class ConcreteProduct1 implements Product {
@@ -28,18 +33,18 @@ class ConcreteProduct2 implements Product {
     }
 }
 //抽象工厂：提供了厂品的生成方法
-interface AbstractFactorys {
-     Product newProduct();
+interface AbstractFactory {
+    public Product newProduct();
 }
 //具体工厂1：实现了厂品的生成方法
-class ConcreteFactory1 implements AbstractFactorys {
+class ConcreteFactory1 implements AbstractFactory {
     public Product newProduct() {
         System.out.println("具体工厂1生成-->具体产品1...");
         return new ConcreteProduct1();
     }
 }
 //具体工厂2：实现了厂品的生成方法
-class ConcreteFactory2 implements AbstractFactorys {
+class ConcreteFactory2 implements AbstractFactory {
     public Product newProduct() {
         System.out.println("具体工厂2生成-->具体产品2...");
         return new ConcreteProduct2();
