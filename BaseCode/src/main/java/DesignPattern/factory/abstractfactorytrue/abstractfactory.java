13c8e225ec173b02133edab32ddde02ee7c60ff4
zhao1 <eda123@qq.com>
2020-11-09 19:42:55 +0800

    2020-11-9 001

---
+++ BaseCode/src/main/java/DesignPattern/factory/abstractfactorytrue/abstractfactory.java
@@ -0,0 +1,180 @@
+package DesignPattern.factory.abstractfactorytrue;
+
+
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * 产品族  动物/植物        等级一
+ * 新种类的产品    动物/牛   等级二
+ */
+
+/**
+ * 抽象工厂模式的扩展有一定的“开闭原则”倾斜性：
+ * ！！！当增加一个新的产品族时只需增加一个新的具体工厂，不需要修改原代码，满足开闭原则。
+ * ！！！当产品族中需要增加一个新种类的产品时，则所有的工厂类都需要进行修改，不满足开闭原则。
+ *
+ * ！！！如下如果增加一个动物狗的话 只需要再加一个工厂 工厂内创建狗
+ * ！！！如果增加一个新种类如 非生物 则所有的工厂都要增加 new非生物 这个方法 不满足开闭原则
+ *
+ * 另一方面，当系统中只存在一个等级结构的产品时，抽象工厂模式将退化到工厂方法模式。
+ */
+
+/**
+ * 工厂可选择
+ *
+ */
+
+public class abstractfactory {
+    public static void main(String[] args) {
+        try {
+            Farm f = new SGfarm();//工厂可选
+            Animal a= f.newAnimal();//工厂内的new产品方法可可选
+            Plant p = f.newPlant();
+            a.show();
+            p.show();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+//抽象产品：动物类
+interface Animal {
+    public void show();
+}
+
+//具体产品：马类
+class Horse implements Animal {
+    JScrollPane sp;
+    JFrame jf = new JFrame("抽象工厂模式测试");
+
+    public Horse() {
+        Container contentPane = jf.getContentPane();
+        JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(1, 1));
+        p1.setBorder(BorderFactory.createTitledBorder("动物：马"));
+        sp = new JScrollPane(p1);
+        contentPane.add(sp, BorderLayout.CENTER);
+        JLabel l1 = new JLabel(new ImageIcon("src/A_Horse.jpg"));
+        p1.add(l1);
+        jf.pack();
+        jf.setVisible(false);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//用户点击窗口关闭
+    }
+
+    public void show() {
+        jf.setVisible(true);
+    }
+}
+
+//具体产品：牛类
+class Cattle implements Animal {
+    JScrollPane sp;
+    JFrame jf = new JFrame("抽象工厂模式测试");
+
+    public Cattle() {
+        Container contentPane = jf.getContentPane();
+        JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(1, 1));
+        p1.setBorder(BorderFactory.createTitledBorder("动物：牛"));
+        sp = new JScrollPane(p1);
+        contentPane.add(sp, BorderLayout.CENTER);
+        JLabel l1 = new JLabel(new ImageIcon("src/A_Cattle.jpg"));
+        p1.add(l1);
+        jf.pack();
+        jf.setVisible(false);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//用户点击窗口关闭
+    }
+
+    public void show() {
+        jf.setVisible(true);
+    }
+}
+
+//抽象产品：植物类
+interface Plant {
+    public void show();
+}
+
+//具体产品：水果类
+class Fruitage implements Plant {
+    JScrollPane sp;
+    JFrame jf = new JFrame("抽象工厂模式测试");
+
+    public Fruitage() {
+        Container contentPane = jf.getContentPane();
+        JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(1, 1));
+        p1.setBorder(BorderFactory.createTitledBorder("植物：水果"));
+        sp = new JScrollPane(p1);
+        contentPane.add(sp, BorderLayout.CENTER);
+        JLabel l1 = new JLabel(new ImageIcon("src/P_Fruitage.jpg"));
+        p1.add(l1);
+        jf.pack();
+        jf.setVisible(false);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//用户点击窗口关闭
+    }
+
+    public void show() {
+        jf.setVisible(true);
+    }
+}
+
+//具体产品：蔬菜类
+class Vegetables implements Plant {
+    JScrollPane sp;
+    JFrame jf = new JFrame("抽象工厂模式测试");
+
+    public Vegetables() {
+        Container contentPane = jf.getContentPane();
+        JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(1, 1));
+        p1.setBorder(BorderFactory.createTitledBorder("植物：蔬菜"));
+        sp = new JScrollPane(p1);
+        contentPane.add(sp, BorderLayout.CENTER);
+        JLabel l1 = new JLabel(new ImageIcon("src/P_Vegetables.jpg"));
+        p1.add(l1);
+        jf.pack();
+        jf.setVisible(false);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//用户点击窗口关闭
+    }
+
+    public void show() {
+        jf.setVisible(true);
+    }
+}
+
+//抽象工厂：农场类
+interface Farm {
+    public Animal newAnimal();
+
+    public Plant newPlant();
+}
+
+//具体工厂：韶关农场类
+class SGfarm implements Farm {
+    public Animal newAnimal() {
+        System.out.println("新牛出生！");
+        return new Cattle();
+    }
+
+    public Plant newPlant() {
+        System.out.println("蔬菜长成！");
+        return new Vegetables();
+    }
+}
+
+//具体工厂：上饶农场类
+class SRfarm implements Farm {
+    public Animal newAnimal() {
+        System.out.println("新马出生！");
+        return new Horse();
+    }
+
+    public Plant newPlant() {
+        System.out.println("水果长成！");
+        return new Fruitage();
+    }
+}
