a080090bc240ab4b7b448f789bcf81d5f8c6f302
zhao1 <eda123@qq.com>
2020-12-25 10:35:04 +0800

    microservice

---
+++ eurekatest/servers-common/src/main/java/com/entity/User.java
@@ -0,0 +1,10 @@
+package com.entity;
+
+import java.util.Date;
+
+public class User {
+    private String name;
+    private Integer age;
+    private Date createTime;
+    private Date updateTime;
+}
